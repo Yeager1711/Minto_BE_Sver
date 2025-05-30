ae0375661b92adeaad65b82a39602c29667145e2
Yeager1711 <huynhnam171102@gmail.com>
2025-05-30 11:26:52 +0700

    add file

--- src/imagekit/imagekit.controller.ts
+++ src/imagekit/imagekit.controller.ts
@@ -1,7 +1,6 @@
-// src/imagekit/imagekit.controller.ts
 import { Controller, Get } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import ImageKit from 'imagekit'; // ✅ dùng đúng với esModuleInterop
+import ImageKit from 'imagekit';
 import { v4 as uuidv4 } from 'uuid';
 
 @Controller('imagekit')
@@ -10,9 +9,9 @@ export class ImageKitController {
 
         constructor(private configService: ConfigService) {
                 this.imagekit = new ImageKit({
-                        publicKey: 'public_MWz6Jz8dBiCgIo0k5lWmcvBZjqk=',
-                        privateKey: 'private_a0k0wfg9Qa3EyaqNLloatHug3+c=',
-                        urlEndpoint: 'https://ik.imagekit.io/zawkrzrax',
+                        publicKey: this.configService.get<string>('IMAGEKIT_PUBLIC_KEY') || '',
+                        privateKey: this.configService.get<string>('IMAGEKIT_PRIVATE_KEY') || '',
+                        urlEndpoint: this.configService.get<string>('IMAGEKIT_URL_ENDPOINT') || '',
                 });
         }
 
